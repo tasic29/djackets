bdf2a4b37e9457e89606b0d6802ff749d6b364e0
Milos Tasic <tasic29@gmail.com>
2025-02-12 08:26:15 +0100

    add order component

--- djackets_vue/src/views/MyAcoount.vue
+++ djackets_vue/src/views/MyAcoount.vue
@@ -14,11 +14,11 @@
       <div class="column is-12">
         <h2 class="subtitle">My orders</h2>
 
-        <!-- <OrderSummary
+        <OrderSummary
           v-for="order in orders"
           v-bind:key="order.id"
           v-bind:order="order"
-        /> -->
+        />
       </div>
     </div>
   </div>
@@ -27,12 +27,12 @@
 <script>
 import axios from "axios";
 
-// import OrderSummary from "@/components/OrderSummary.vue";
+import OrderSummary from "@/components/OrderSummary.vue";
 
 export default {
   name: "MyAccount",
   components: {
-    // OrderSummary,
+    OrderSummary,
   },
   data() {
     return {
